51cfd9f1893792ee5ddd41333f57625022df5b4e
gbrParanhos <gbrparanhos@gmail.com>
2024-06-16 14:42:02 -0300

    Função de envio de mensagem criado

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const urlParticipants = 'https://mock-api.driven.com.br/api/v6/uol/participants/b29842f4-b480-49f8-94db-fcfd7c422827'
 const urlMessages = 'https://mock-api.driven.com.br/api/v6/uol/messages/b29842f4-b480-49f8-94db-fcfd7c422827'
+const urlStatus = 'https://mock-api.driven.com.br/api/v6/uol/status/b29842f4-b480-49f8-94db-fcfd7c422827'
 const overlay = document.querySelector('.overlay');
 const settings = document.querySelector('.settings');
 let peopleSelected = document.querySelector('.all-peoples');
@@ -11,7 +12,7 @@ const openSettings = () => {
   overlay.classList.remove('hidden');
   settings.classList.remove('hidden');
 }
-
+  
 const closeSettings = () => {
   overlay.classList.add('hidden');
   settings.classList.add('hidden');
@@ -19,25 +20,30 @@ const closeSettings = () => {
 
 const selectPeople = (element) => {
   if (element !== peopleSelected) {
-    peopleSelected.querySelector('img').classList.add('hidden')
+    peopleSelected.querySelector('svg').classList.add('hidden')
     peopleSelected = element
     console.log(peopleSelected)
-    peopleSelected.querySelector('img').classList.remove('hidden')
+    peopleSelected.querySelector('svg').classList.remove('hidden')
   }
 }
 
 const selectVisibility = (element) => {
   if (element !== visibilitySelected) {
-    visibilitySelected.querySelector('img').classList.add('hidden')
+    visibilitySelected.querySelector('svg').classList.add('hidden')
     visibilitySelected = element
     console.log(visibilitySelected)
-    visibilitySelected.querySelector('img').classList.remove('hidden')
+    visibilitySelected.querySelector('svg').classList.remove('hidden')
   }
 }
 
 const searchMessages = () => {
-  const promisse = axios.get(urlMessages,)
-  .then(res => console.log(res))
+  const promisse = axios.get(urlMessages,);
+  promisse.then(res => console.log(res));
+}
+
+const sendMessage = (event) => {
+  event.preventDefault()
+  console.log(event)
 }
 
 searchMessages()
